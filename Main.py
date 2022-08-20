5484e976f9845d2313fc7d7335ace726922e1392
Karlos Chevez <gregorych99@gmail.com>
2022-08-20 12:12:46 -0500

    Nuevos Datos 1

---
+++ Main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from IMC_Clases import *
+
+
+if __name__== '__main__':
+    #ABRIMOS EL MENU PRINICIPAL DE LA APLICACION
+    VentanaMenu = Tk()
+    aplicacion=menuPrincipal(VentanaMenu)
+    VentanaMenu.mainloop()
